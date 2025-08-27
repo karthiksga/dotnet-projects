a508fac9e369a762569a3432bb350aa4db84f65a
karthiksga <karthiksg.eee@gmail.com>
2025-08-27 17:58:26 -0400

    AccessModifierTest

---
+++ AccessModifierTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace AccessModifierTest
+{
+    public class Class1
+    {
+        protected string ProtectedProperty { get; set; } = "TEst";
+        protected internal string ProtectedInternalProperty { get; set; } = "Test2";
+
+        public Class1()
+        {
+            
+        }
+        protected void ProtectedMethod()
+        {
+            // This method can only be accessed within this class or by derived classes.
+        }
+    }
+
+    public class Class2
+    {
+        public Class2()
+        {
+            var class1 = new Class1();
+            class1.protectedProerty = "Test"; // This will cause a compile-time error because ProtectedProperty is not accessible here.
+            class1.ProtectedInternalProperty = "Test3"; // This is valid because Class2 is in the same assembly as Class1.
+
+        }
+    }
+}
